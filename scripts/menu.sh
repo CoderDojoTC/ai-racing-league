b14aca876b171ac9939cd246bd7c92fa4f35552b
Dan <Dan>
2020-01-19 11:03:44 -0600

    Added new menu text

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -12,42 +12,62 @@ pause(){
 one(){
 	echo "Calibrate Steering"
 	echo "Make sure that you can communicate with the I2C bus and turn your ASC swtich to ON"
+	echo "Center is 330, Left is 420 and Right is 240"
         pause
+	echo "donkey calibrate --channel 1 --bus=1"
 }
  
 # do something in two()
 two(){
 	echo "Calibrate Throtte"
+	echo "Make sure your car is on a testing stand.  The wheels will spin very fast."
+	echo "Center is 330, Left is 420 and Right is 240"
         pause
+	echo "donkey calibrate --channel 0 --bus=1"
 }
 
 three(){
 	echo "Verify I2C Communication"
+	echo "Here we check to see if we can communicate with the I2C Bus"
+	echo "ON the Nano, the I2C bus we are using is bus 1"
         pause
+	echo "i2cdetect -r -y 1"
 }
 
 four(){
 	echo "Drive with Joystick"
         pause
+	echo "python manage.py drive --js"
 }
 
 five(){
 	echo "Drive with Web Page"
         pause
+	echo "python manage.py drive"
+	echo "after this you can go to http://hostname:8887 drive"
 }
 
 six(){
 	echo "Compress Tub to Zip File"
+	echo "You will need to select one or more folders and run this command"
+	echo "zip -r mytubs.zip data/tub1 data/tub1"
         pause
 }
 
 seven(){
 	echo "Send Zip File to GPU Server"
+	echo "To transfer the zip file to the GPU server you will need to know the hostname of the GPU server"
+	echo "Try to ping arl1.local or arl2.local"
+	echo "You can then use the secure copy command"
+	echo "scp mytubs.zip arl2.local:/tmp/dans-tubs.zip"
+	echo "If you can't connect to the server, copy the files to a jump drive"
+	echo "cp mytubs.zip /local-path-to-flash-drive"
         pause
 }
 
 eight(){
 	echo "Get Model from GPU Server"
+	echo "scp arl2.local:/tmp/dans-model.h5 ~/mycar/models"
         pause
 }
 
